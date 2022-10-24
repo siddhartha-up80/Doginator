ade6a531ff30d6b496adad4aa3311e3943387862
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2022-10-24 10:12:18 +0530

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import hero from "../assets/images/hero.png";
-import AutoTyping, { BlinkCursor } from 'react-auto-typing'
+
 
 const Hero = () => {
   const social_media = [
@@ -18,18 +18,7 @@ const Hero = () => {
         <div className="md:text-left text-center">
           <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
             <span className="text-cyan-600 md:text-5xl text-5xl">
-             <span className="text-white text-3xl">Want a full time dog?</span>  <br/> <span className="text-4xl"><AutoTyping
-      active // <boolean>
-      textRef= 'Gautam Premium Services' // <string>
-      writeSpeed={150} // <number>
-      deleteSpeed={150} // <number>
-      delayToWrite={1000} // <number>
-      delayToDelete={2000} // <number>
-    />
-    <BlinkCursor
-      active // <boolean>
-      blinkSpeed={500} // <number>
-    /></span>
+             <span className="text-white text-3xl">Want a full time dog?</span>  <br/> <span className="text-4xl">Gautam Premium Services</span>
               <br />
             </span>
             
@@ -39,7 +28,7 @@ const Hero = () => {
             FIR Expert, Dog Food Expert, Dogs Security Provider
           </h4>
           <a href="#contact"><button className="btn-primary mt-8">Contact Me</button></a>
-          <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5 ml-10">
+          <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5 md:ml-10">
             {social_media?.map((icon) => (
               <div
                 key={icon}
